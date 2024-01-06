485a1262e18ea0ecc304ac52c60eac9649614a7e
travis <travisjb0@gmail.com>
2024-01-06 13:06:29 -0800

    fixed image background issue

--- businesssite/app/page.tsx
+++ businesssite/app/page.tsx
@@ -6,30 +6,30 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className='w-screen h-screen relative'>
-      <div className='flex items-center w-full h-full bg-cover bg-center' style={{backgroundImage: "url(/main-bg.wbp)"}}>
+      <div className='flex items-center w-full h-full bg-cover bg-center' style={{backgroundImage: "url(/main-bg.webp)"}}>
             <div className='pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]'>
-                <h1 className='text-[50px] text-black font-semibold'>
+                <h1 className='text-[50px] text-white font-semibold'>
                   Make anything possible with
                   <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>
                     {" "}
                     Web Development
                   </span>
                 </h1>
-                <p className='text-gray-200 hidden md:block'>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, fuga!
+                <p className='text-white hidden md:block'>
+                  Aspiring to Create web Applications That Excell
                 </p>
                 <div className='flex-col md:flex-row hidden md:flex gap-5'>
-                  <Link href="/my-skills" className='rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+                  <Link href="/ourProcess" className='rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
                       Learn More
                   </Link>
-                  <Link href="/my-skills" className='rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+                  <Link href="/technology" className='rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
                       <div className='absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20' />
-                      Contact Me
+                      Our Technology
                   </Link>
 
-                  <Link href="/my-skills" className='rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
+                  <Link href="/contact" className='rounded-[20px] bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]'>
                       <div className='absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hover:opacity-20' />
-                      Contact Me
+                      Contact Us
                   </Link>
 
                 </div>
