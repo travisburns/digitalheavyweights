96ec0a585649a50432178fb924e3d67d8bc70e2d
travis <travisjb0@gmail.com>
2024-02-24 02:23:56 -0800

    Added better design for project cards on Mobile

--- businesssite/app/my-projects/page.tsx
+++ businesssite/app/my-projects/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import ProjectCard from '@/components/ProjectCard'
-import { Projects } from '@/constants'
+import ProjectCard from '../../components/ProjectCard'
+
 import React from 'react'
 
 const Page = () => {
@@ -9,12 +9,19 @@ const Page = () => {
     <div
     style={{backgroundImage: "url(/mountains.jpg)"}}
      className='w-screen h-screen items-center justify-center bg-center bg-cover'>
-      <div className=' gap-5 max-w-[90%] max-h-[90%]'>
-        
+      <div className='relative top-[10rem] w-[80%] text-center'>
+        <ProjectCard title='WeatherApplication' description='Display Weather' imageSrc='/bg-3.png' />
+        <ProjectCard title='InteractiveMap' description='Display Weather' imageSrc='/bg-3.png' />
+        <ProjectCard title='MuseFind' description='Display Weather' imageSrc='/bg-3.png' />
+        <ProjectCard title='R&WEngineering' description='Display Weather' imageSrc='/bg-3.png' />
+        <ProjectCard title='GemShop' description='Display Weather' imageSrc='/bg-3.png' />
+        <ProjectCard title='Boundless' description='Display Weather' imageSrc='/bg-3.png' />
+        <ProjectCard title='EccomerceDashBoard' description='Display Weather' imageSrc='/bg-3.png' />
       
       </div>
     </div>
   )
+
 }
 
 export default Page
--- businesssite/components/ProjectCard.tsx
+++ businesssite/components/ProjectCard.tsx
@@ -1,55 +1,39 @@
 "use client"
-import React, { useState } from 'react'
-import {motion} from 'framer-motion'
+ 
 
-interface Props {
-    image: string;
-    title: string;
-    text: string;
+import React from 'react'
+import Image from "next/image";
+interface projectCardProps {
+  title: string;
+  description: string;
+  imageSrc: string;
 }
 
-const ProjectCard = ({ image, title, text}: Props) => {
-    const [isFlipped, setIsFlipped] = useState(false)
-    const [isAnimating, setIsAnimating] = useState(false)
 
-    function handleFlip() {
-        if(!isAnimating) {
-            setIsFlipped(!isFlipped)
-            setIsAnimating(true)
-        }
-    }
+const ProjectCard: React.FC<projectCardProps> = ({imageSrc, title, description}) => {
+
+
   return (
-    <div
-    onClick={handleFlip}
-    className='w-[450px] h-[280px] rounded-md cursor-pointer'>
-        <motion.div
-        className='flip-card-inner w-full h-full'
-        initial={false}
-        animate={{rotateY: isFlipped ? 180 : 360}}
-        transition={{ duration: 0.6, animationDirection: 'normal'}}
-        onAnimationComplete={() => setIsAnimating(false)}
-        >
-            <div
-            style={{backgroundImage: `url(${image})`}}
-            className='w-full h-full group relative flip-card-front bg-cover bg-center text-white rounded-lg p-4'>
-                    <div  className='absolute inset-0 w-full h-full rounded-md bg-black opacity-0 group-hover:opacity-40'/>
-                    <div className='absolute inset-0 w-full h-full text-[20px] pb-10 hidden group-hover:flex items-center z-[20] justify-center'>
-                    Learn more &gt;
-                    </div>
-            </div>
-            <div
-            style={{backgroundImage: `url(${image})`}}
-            className='w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4'>
-                    <div  className='absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]'/>
-                  <div className='flex flex-col gap-20 py-3 z-[30]'>
-                    <h1 className='text-whote text-2xl font-semibold'>{title}</h1>
-                    <p className='text-gray-200 text-[20px]'>
-                        {text}
-                    </p>
-                  </div>
-            </div>
-        </motion.div>
+    
+    <div className='bg-slate-950 mb-5 w-[100%]'>
+       <a href="#" className="bg-slate-950 border border-gray-200   shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 relative ">
+       
+    <div className="flex flex-col justify-between  ">
+        <div className=''>
+        <Image alt='' width={300} height={150} src={imageSrc}></Image> 
+        <h5 className="mb-2 text-[2rem] font-bold tracking-tight text-white dark:text-white">{title}</h5>
+        </div>
+        
+        <p className=" text-[1rem] text-center text-white dark:text-gray-400">{description}</p>
+
+    
     </div>
+</a>
+       <div/>
+    </div>
+
+
+
   )
 }
 
