a8c7874fc49b4c47736faefdacd7a4165b5aa922
travisburns <travisjb0@gmail.com>
2024-07-06 18:17:06 -0700

    for hosting edits

--- businesssite/next.config.js
+++ businesssite/next.config.js
@@ -1,11 +1,18 @@
-module.exports = {
-   
-    async rewrites() {
-      return [
-        {
-          source: '/our-process/:step', // Dynamic route pattern
-          destination: '/our-process/step', // Actual page to render
-        },
-      ]
-    },
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  experimental: {
+    outputFileTracingRoot: __dirname,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/our-process/:step', // Dynamic route pattern
+        destination: '/our-process/step', // Actual page to render
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
