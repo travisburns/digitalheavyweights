c98fc5537e926be7ba2d11246ea53d80c54c1dbf
travisburns <travisjb0@gmail.com>
2025-09-29 15:13:21 -0700

    added changes to duplicate exp

--- businesssite/constants/experienceData.ts
+++ businesssite/constants/experienceData.ts
@@ -3,28 +3,32 @@ export const experience = [
     period: 'APR — JUN 2025',
     title: 'Software Developer Intern',
     company: 'Trifoia',
-    description: 'Developed an access request system using Blazor, Razor, and dependency injection, streamlining onboarding processes. Migrated production database from Azure to MonsterASP.NET with zero downtime and integrated AWS SES for email templates. Resolved 30+ UI/UX issues across 15+ views, enhancing user experience.',
+    description:
+      'Developed an access request system using Blazor, Razor, and dependency injection. Migrated production database from Azure to MonsterASP.NET with zero downtime. Integrated AWS SES for automated email templates and resolved 30+ UI/UX issues across 15+ views.',
     tech: ['C#', 'Blazor', 'ASP.NET', 'SQL Server', 'Azure', 'AWS SES', 'Radzen']
   },
   {
     period: 'FEB — JUN 2025',
     title: 'Software Developer (Capstone Project)',
     company: 'Lane Community College',
-    description: 'Built a peer review platform in a 3-person Agile team with sprint ownership and GitHub workflows. Designed backend architecture using Entity Framework and MySQL, implementing role-based authentication with ASP.NET Identity for Admin, Instructor, and Student roles.',
+    description:
+      'Built a peer review platform in a 3-person Agile team with sprint ownership and GitHub workflows. Designed backend architecture with Entity Framework and MySQL, and implemented secure role-based authentication with ASP.NET Identity.',
     tech: ['C#', 'ASP.NET', 'Entity Framework', 'MySQL', 'ASP.NET Identity', 'GitHub']
   },
-   {
+  {
     period: 'SEP — DEC 2024',
-    title: 'Data Analyst/ Associate Population Health Strategist',
-    company: 'Pacific Source Health Plans',
-    description: 'Built a peer review platform in a 3-person Agile team with sprint ownership and GitHub workflows. Designed backend architecture using Entity Framework and MySQL, implementing role-based authentication with ASP.NET Identity for Admin, Instructor, and Student roles.',
-    tech: ['Excel', 'Tablue', 'Visual Basic',]
+    title: 'Data Analyst / Associate Population Health Strategist',
+    company: 'PacificSource Health Plans',
+    description:
+      'Organized and cleaned 2,000+ client reports into a centralized system, saving 50% of data retrieval time. Generated Medicare and Medicaid reports with Tableau, improving preparation efficiency by 30%. Automated Excel processes with Visual Basic macros, reducing manual reporting work by 30%.',
+    tech: ['Excel', 'Tableau', 'Visual Basic']
   },
-   {
+  {
     period: 'MAR 2022 — DEC 2023',
     title: 'Web Developer',
     company: 'Freelance',
-    description: 'Built a variety of web applications for client work during this time.',
-    tech: ['html', 'css', 'Javascript', 'Typescript', 'NextJS', 'MongoDB', 'Tailwind']
+    description:
+      'Built and deployed a variety of client web applications, focusing on responsive design, performance optimization, and scalable back-end architecture.',
+    tech: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'Next.js', 'MongoDB', 'Tailwind']
   }
 ];
